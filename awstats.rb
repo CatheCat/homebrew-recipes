1434a9ae7d7272e6ba77e6ac5bce253b1b8bd479
catherine <athena19911104@gmail.com>
2017-07-11 16:15:47 +0800

    Update awstats.rb

--- awstats.rb
+++ awstats.rb
@@ -6,7 +6,7 @@ require "formula"
 
 class Awstats < Formula
   homepage "http://awstats.sourceforge.net/"
-  url "http://prdownloads.sourceforge.net/awstats/awstats-7.2.tar.gz"
+  url "http://prdownloads.sourceforge.net/awstats/awstats-7.5.tar.gz"
   sha256 "740cb46ac5a7befb72ff0b80d0e53c6fe3560700208bce18f4fb5b2b5e275791"
 
   def install
